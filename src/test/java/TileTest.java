c718ac31bc4f212aa84e26847f277b7158d35a45
Harry <harry.johnson5481@gmail.com>
2022-10-14 15:24:39 +0100

    Changed from @test to include extra things that seem to be needed???

--- src/test/java/TileTest.java
+++ src/test/java/TileTest.java
@@ -5,46 +5,42 @@ public class TileTest {
 
     Tile testTile = new Tile();
 
-    @Test
+    @org.junit.Test
     public void testTileIsHidden() {
         Assertions.assertEquals(true, testTile.getHidden(), "Tiles generate as visible");
     }
-    @Test
+    @org.junit.Test
     public void testTileIsUnflagged() {
         Assertions.assertEquals(false, testTile.getFlag(), "tiles generate as unflagged");
     }
-    @Test
+    @org.junit.Test
     public void testFlagTile() {
         testTile.setFlag();
         Assertions.assertEquals(true, testTile.getFlag(), "tiles generate as flagged");
         testTile.setFlag();
         Assertions.assertEquals(false, testTile.getFlag(), "tiles generate as flagged");
     }
-    @Test
+    @org.junit.Test
     public void testAddNearbyMine() {
         testTile.addNearbyMine();
         Assertions.assertEquals(1,testTile.getValue());
     }
-    @Test
+    @org.junit.Test
     public void testGetValue() {
         Assertions.assertEquals(0, testTile.getValue(), "tiles generate as flagged");
     }
-    @Test
+    @org.junit.Test
     public void testTileIsNotMine() {
         Assertions.assertEquals(false, testTile.getMine(), "tiles generated as mine");
     }
-    @Test
+    @org.junit.Test
     public void testTileSetHidden() {
         testTile.setHidden(false);
         Assertions.assertEquals(false, testTile.getHidden(), "unflagged tile flagged");
         testTile.setHidden(true);
         Assertions.assertEquals(true, testTile.getHidden(), "flagged tile not unflagged");
     }
-    @Test
-    public void testTileGetName() {
-        Assertions.assertEquals("0", testTile.getName(), "tiles set to be shown");
-    }
-    @Test
+    @org.junit.Test
     public void testTileSetName() {
         testTile.setName("A");
         Assertions.assertEquals("A", testTile.getName(), "tiles set to be shown");
